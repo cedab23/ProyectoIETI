5028488142c5c79978aeb61feca70c0efdd586c5
andres Rodriguez <rodriguezandres160918@gmail.com>
2025-09-10 09:02:13 -0500

    AHora si

---
+++ src/main/java/com/ieti/proyectoieti/ProyectoietiApplication.java
@@ -0,0 +1,13 @@
+package com.ieti.proyectoieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoietiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoietiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ieti/proyectoieti/ProyectoietiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieti.proyectoieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoietiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
